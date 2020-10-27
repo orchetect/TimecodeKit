428b99909f697dcf8bd5a67003c06b58f7eb4c6d
Steffan Andrews <stef@orchetect.com>
2020-10-26 23:04:25 -0700

    Added Timecode.converted(to:)

---
+++ Tests/TimecodeKit-Unit-Tests/Unit Tests/Timecode Conversion Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Timecode Conversion Tests.swift
+//  TimecodeKitTests
+//
+//  Created by Steffan Andrews on 2020-10-26.
+//  Copyright © 2020 Steffan Andrews. All rights reserved.
+
+import XCTest
+@testable import TimecodeKit
+
+class Timecode_UT_Conversion: XCTestCase {
+	
+	override func setUp() { }
+	override func tearDown() { }
+	
+	func testConverted1to1() {
+		
+		// baseline check:
+		// ensure frame rates convert to themselves
+		
+		Timecode.FrameRate.allCases.forEach {
+			
+			let tc = TCC(h: 1)
+				.toTimecode(at: $0)
+			
+			let convertedTC = tc?.converted(to: $0)
+			
+			XCTAssertEqual(tc, convertedTC)
+			
+		}
+		
+	}
+	
+}
