a3c8bca8cbffe2cfc155972040852300adaf20a1
Steffan Andrews <stef@orchetect.com>
2021-01-09 23:34:23 -0800

    Added FrameRate.CompatibleGroup and related methods

---
+++ Sources/TimecodeKit/FrameRate/FrameRate CompatibleGroup.swift
@@ -0,0 +1,98 @@
+//
+//  FrameRate CompatibleGroup.swift
+//  TimecodeKit
+//
+//  Created by Steffan Andrews on 2021-01-09.
+//  Copyright © 2021 Steffan Andrews. All rights reserved.
+//
+
+import Foundation
+
+extension Timecode.FrameRate {
+	
+	/// Enum describing compatible groupings of frame rates.
+	///
+	/// - note: These are intended for internal logic and not for end-user user interface.
+	public enum CompatibleGroup {
+		case NTSC
+		case NTSC_drop
+		case ATSC
+		case ATSC_drop
+		
+		/// Constants table of `FrameRate` groups that share HH:MM:SS alignment between them, while only frames value may differ.
+		///
+		/// These groupings assert that they are interchangeable in so much as hours, minutes, and seconds values will always be identical between them at the same elapsed real time, but only frames value may differ.
+		///
+		/// For example, at the same point of elapsed real time, 30 and 60 fps are compatible with each other, but 29.97 is not:
+		///
+		/// - 01:00:00:00 @ 30 fps
+		/// - 01:00:00:00 @ 60 fps
+		/// - 00:59:56:12 @ 29.97 fps
+		public static var table: [CompatibleGroup : [Timecode.FrameRate]] =
+			[
+				
+				.NTSC: [
+					._23_976,
+					._24_98,
+					._29_97,
+					._47_952,
+					._59_94,
+					._119_88
+				],
+				
+				.NTSC_drop: [
+					._29_97_drop,
+					._59_94_drop,
+					._119_88_drop
+				],
+				
+				.ATSC: [
+					._24,
+					._25,
+					._30,
+					._48,
+					._50,
+					._60,
+					._100,
+					._120
+				],
+				
+				.ATSC_drop: [
+					._30_drop,
+					._60_drop,
+					._120_drop
+				]
+				
+			]
+		
+	}
+	
+	/// Returns the frame rate's `CompatibleGroup` categorization.
+	public var compatibleGroup: CompatibleGroup {
+		
+		// Force-unwrap here will never crash because the unit tests ensure the table contains all Timecode.FrameRate cases.
+		
+		Self.CompatibleGroup.table
+			.first(where: { $0.value.contains(self) })!
+			.key
+		
+	}
+	
+	/// Returns true if the source `FrameRate` shares a compatible grouping with the passed `timecode`.
+	///
+	/// For example, at the same point of elapsed real time, 30 and 60 fps are compatible with each other, but 29.97 is not:
+	///
+	/// - 01:00:00:00 @ 30 fps
+	/// - 01:00:00:00 @ 60 fps
+	/// - 00:59:56:12 @ 29.97 fps
+	public func isCompatible(with timecode: Self) -> Bool {
+		
+		Self.CompatibleGroup.table
+			.values
+			.first(where: { $0.contains(self) })?
+			.contains(timecode)
+			?? false
+		
+	}
+	
+}
