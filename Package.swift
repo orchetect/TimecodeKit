35571fc9737a10a07d27683088161431f8eed32c
Steffan Andrews <stef@orchetect.com>
2021-01-18 23:08:39 -0800

    Updated dependency min versions

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
     dependencies: [
 		
 		// utility/support code:
-		.package(url: "https://github.com/orchetect/OTCore", from: "1.0.0"),
+		.package(url: "https://github.com/orchetect/OTCore", from: "1.1.0"),
 		
 		// console progress module for dev tests:
-		.package(url: "https://github.com/orchetect/SegmentedProgress", from: "1.0.0")
+		.package(url: "https://github.com/orchetect/SegmentedProgress", from: "1.0.1")
 		
     ],
 	
