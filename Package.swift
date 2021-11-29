e5af4c88b5f3b50a180a05b8c1aada0320abf51b
Steffan Andrews <stef@orchetect.com>
2021-11-29 04:34:14 -0800

    Updated dependency versions

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     
     dependencies: [
         // boilerplate:
-        .package(url: "https://github.com/orchetect/OTCore", from: "1.1.21")
+        .package(url: "https://github.com/orchetect/OTCore", from: "1.1.22")
         
         // used only for Dev tests, not part of regular unit test
         //.package(url: "https://github.com/orchetect/SegmentedProgress", from: "1.0.1")
