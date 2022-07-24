7bb7589dd811cdfdf291aec7c21d5c0b6c7d67ce
Steffan Andrews <stef@orchetect.com>
2022-07-23 19:30:27 -0700

    Moved `Codable` unit tests to their own file

--- Tests/TimecodeKit-Unit-Tests/Unit Tests/Timecode Tests.swift
+++ Tests/TimecodeKit-Unit-Tests/Unit Tests/Timecode Tests.swift
@@ -13,46 +13,11 @@ class Timecode_UT_Tests: XCTestCase {
     override func setUp() { }
     override func tearDown() { }
     
-    func testCodable() throws {
+    func testEmpty() throws {
         
-        // set up JSON coders with default settings
+        // no tests in this file
         
-        let encoder = JSONEncoder()
-        let decoder = JSONDecoder()
-        
-        try Timecode.FrameRate.allCases.forEach {
-            
-            // set up a timecode object that has all non-defaults
-            
-            let tc = try "1 12:34:56:11.85"
-                .toTimecode(at: $0,
-                            limit: ._100days,
-                            base: ._100SubFrames,
-                            format: [.showSubFrames])
-            
-            // encode
-            
-            let encoded = try encoder.encode(tc)
-            
-            // decode
-            
-            let decoded = try decoder.decode(Timecode.self, from: encoded)
-            
-            // compare original to reconstructed
-            
-            XCTAssertEqual(tc, decoded)
-            
-            XCTAssertEqual(tc.days, decoded.days)
-            XCTAssertEqual(tc.hours, decoded.hours)
-            XCTAssertEqual(tc.minutes, decoded.minutes)
-            XCTAssertEqual(tc.seconds, decoded.seconds)
-            XCTAssertEqual(tc.frames, decoded.frames)
-            XCTAssertEqual(tc.frameRate, decoded.frameRate)
-            XCTAssertEqual(tc.upperLimit, decoded.upperLimit)
-            XCTAssertEqual(tc.subFramesBase, decoded.subFramesBase)
-            XCTAssertEqual(tc.stringFormat, decoded.stringFormat)
-            
-        }
+        XCTAssertTrue(true)
         
     }
     
