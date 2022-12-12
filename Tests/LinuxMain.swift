1e1c71351e6a65a4ac6b9d75a6b9eadd9aee0369
Steffan Andrews <stef@orchetect.com>
2022-12-11 20:59:24 -0800

    Removed LinuxMain.swift - stale and no longer needed

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-//
-//  LinuxMain.swift
-//  TimecodeKit • https://github.com/orchetect/TimecodeKit
-//  © 2022 Steffan Andrews • Licensed under MIT License
-//
-
-fatalError("Run the tests with `swift test --enable-test-discovery`")
