51fbd192681231785aa9638d479a8f404179e6b2
Steffan Andrews <stef@orchetect.com>
2023-03-21 18:43:10 -0700

    Added unit test

---
+++ Tests/TimecodeKit-Unit-Tests/Unit Tests/AVFoundation Extensions/AVFoundation Utils Tests.swift
@@ -0,0 +1,42 @@
+//
+//  AVFoundation Utils Tests.swift
+//  TimecodeKit • https://github.com/orchetect/TimecodeKit
+//  © 2023 Steffan Andrews • Licensed under MIT License
+//
+
+// AVAssetReader is unavailable on watchOS so we can't support any AVAsset operations
+#if shouldTestCurrentPlatform && canImport(AVFoundation) && !os(watchOS)
+
+import XCTest
+@testable import TimecodeKit
+import AVFoundation
+
+class AVFoundationUtils_Tests: XCTestCase {
+    override func setUp() { }
+    override func tearDown() { }
+    
+    func testCMTimeRange_timecodeRange() throws {
+        let s10 = CMTime(seconds: 10, preferredTimescale: 600)
+        let s9 = CMTime(seconds: 9, preferredTimescale: 600)
+        
+        // valid
+        
+        XCTAssertEqual(
+            try CMTimeRange(start: s10, end: s10).timecodeRange(at: ._30),
+            try Timecode(TCC(s: 10), at: ._30) ... Timecode(TCC(s: 10), at: ._30)
+        )
+        
+        XCTAssertEqual(
+            try CMTimeRange(start: s10, duration: s10).timecodeRange(at: ._30),
+            try Timecode(TCC(s: 10), at: ._30) ... Timecode(TCC(s: 20), at: ._30)
+        )
+        
+        // invalid
+        
+        XCTAssertThrowsError(
+            try CMTimeRange(start: s10, end: s9).timecodeRange(at: ._30)
+        )
+    }
+}
+
+#endif
