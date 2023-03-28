caff40b2838f916385f45706e9960586814a1e16
Steffan Andrews <stef@orchetect.com>
2023-03-27 18:17:05 -0700

    Updated VideoFrameRate unit tests

--- Tests/TimecodeKit-Unit-Tests/Unit Tests/VideoFrameRate/VideoFrameRate Conversions Tests.swift
+++ Tests/TimecodeKit-Unit-Tests/Unit Tests/VideoFrameRate/VideoFrameRate Conversions Tests.swift
@@ -407,8 +407,8 @@ class VideoFrameRate_Conversions_CMTime_Tests: XCTestCase {
         try VideoFrameRate.allCases.forEach {
             let cmTimeSeconds = $0.frameDurationCMTime.seconds
             
-            let oneFrameDuration = try TCC(f: 1)
-                .toTimecode(at: $0.timecodeFrameRate(drop: false)!)
+            let oneFrameDuration = try Timecode.Components(f: 1)
+                .timecode(using: $0.timecodeFrameRate(drop: false)!)
                 .realTimeValue
             
             XCTAssertEqual(
