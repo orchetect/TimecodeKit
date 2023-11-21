fe516e547ce63acd0e84e7bf5c77912687b4f750
Steffan Andrews <stef@orchetect.com>
2023-11-20 17:54:56 -0800

    Added utility properties

---
+++ Sources/TimecodeKit/Utilities/Outsourced/FloatingPoint and Foundation.swift
@@ -0,0 +1,32 @@
+/// ----------------------------------------------
+/// ----------------------------------------------
+/// OTCore/Extensions/Swift/FloatingPoint.swift
+///
+/// Borrowed from OTCore 1.4.14 under MIT license.
+/// https://github.com/orchetect/OTCore
+/// Methods herein are unit tested at their source
+/// so no unit tests are necessary.
+/// ----------------------------------------------
+/// ----------------------------------------------
+
+#if canImport(Foundation)
+
+import Foundation
+
+// MARK: - Digit Places
+
+extension Double {
+    /// **OTCore:**
+    /// Returns the number of digit places of the ``integral`` portion (left of the decimal).
+    var integralDigitPlaces: Int {
+        Decimal(self).integralDigitPlaces
+    }
+    
+    /// **OTCore:**
+    /// Returns the number of digit places of the ``fraction`` portion (right of the decimal).
+    var fractionDigitPlaces: Int {
+        Decimal(self).fractionDigitPlaces
+    }
+}
+
+#endif
