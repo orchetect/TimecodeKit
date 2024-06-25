7959a0b234a5237b693dc8b4dbeeea4c52ff9e54
Steffan Andrews <stef@orchetect.com>
2024-06-24 21:49:30 -0700

    Fixed rare compiler error when building using build pipelines

--- Sources/TimecodeKit/Utilities/URL.swift
+++ Sources/TimecodeKit/Utilities/URL.swift
@@ -8,9 +8,21 @@ import Foundation
 
 extension URL {
     init(temporaryFileWithData data: Data) throws {
-        let url = FileManager.default.temporaryDirectory
+        let url = FileManager.default.temporaryDirectoryCompat
             .appendingPathComponent(UUID().uuidString)
         try data.write(to: url)
         self = url
+    }
+}
+
+extension FileManager {
+    /// Backwards compatible method for retrieving a temporary folder from the system.
+    @_disfavoredOverload
+    var temporaryDirectoryCompat: URL {
+        if #available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *) {
+            return temporaryDirectory
+        } else {
+            return URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        }
     }
 }
