59d408af94f1852a93a419ae5b7177262166d440
Steffan Andrews <stef@orchetect.com>
2024-10-06 15:23:34 -0700

    Added TimecodeUI Example project

---
+++ Examples/TimecodeUI Example/TimecodeUI Example/App/TimecodeUIExampleApp.swift
@@ -0,0 +1,16 @@
+//
+//  TimecodeUIExampleApp.swift
+//  TimecodeKit • https://github.com/orchetect/TimecodeKit
+//  © 2020-2024 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftUI
+
+@main
+struct TimecodeUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
