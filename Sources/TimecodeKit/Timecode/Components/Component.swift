eeab1a832fd26fda0eb4140db7c8dbb07943df1b
Steffan Andrews <stef@orchetect.com>
2024-10-05 21:54:17 -0700

    `Timecode.Component`: Added `next(excluding:)` and `previous(excluding:)` methods

--- Sources/TimecodeKit/Timecode/Components/Component.swift
+++ Sources/TimecodeKit/Timecode/Components/Component.swift
@@ -22,3 +22,47 @@ extension Timecode.Component: Identifiable {
 }
 
 extension Timecode.Component: Sendable { }
+
+// MARK: - Sequence Traversal
+
+extension Timecode.Component {
+    /// Returns the next timecode component in sequence.
+    /// If the component is the last in the sequence, the sequence wraps around and returns the first component.
+    public func next(excluding: [Self] = []) -> Self {
+        let components = Self.allCases.filter { !excluding.contains($0) }
+        
+        precondition(!components.isEmpty)
+        
+        guard let index = components.firstIndex(of: self) else {
+            return components.first!
+        }
+        
+        let offsetIndex = index.advanced(by: 1)
+        
+        if components.indices.contains(offsetIndex) {
+            return components[offsetIndex]
+        } else {
+            return components.first!
+        }
+    }
+    
+    /// Returns the previous timecode component in sequence.
+    /// If the component is the first in the sequence, the sequence wraps around and returns the last component.
+    public func previous(excluding: [Self] = []) -> Self {
+        let components = Self.allCases.filter { !excluding.contains($0) }
+        
+        precondition(!components.isEmpty)
+        
+        guard let index = components.firstIndex(of: self) else {
+            return components.last!
+        }
+        
+        let offsetIndex = index.advanced(by: -1)
+        
+        if components.indices.contains(offsetIndex) {
+            return components[offsetIndex]
+        } else {
+            return components.last!
+        }
+    }
+}
