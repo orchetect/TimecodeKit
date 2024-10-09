2fa54636b2eebe9b68501b796988729000320616
Steffan Andrews <stef@orchetect.com>
2024-10-08 18:29:44 -0700

    `Timecode`: Added new `ComponentRanges` struct

---
+++ Sources/TimecodeKit/Timecode/Components/ComponentRanges.swift
@@ -0,0 +1,53 @@
+//
+//  ComponentRanges.swift
+//  TimecodeKit • https://github.com/orchetect/TimecodeKit
+//  © 2020-2024 Steffan Andrews • Licensed under MIT License
+//
+
+extension Timecode {
+    /// Primitive struct describing component value ranges.
+    public struct ComponentRanges {
+        public var days: ClosedRange<Int>
+        public var hours: ClosedRange<Int>
+        public var minutes: ClosedRange<Int>
+        public var seconds: ClosedRange<Int>
+        public var frames: ClosedRange<Int>
+        public var subFrames: ClosedRange<Int>
+        
+        public init(
+            days: ClosedRange<Int>,
+            hours: ClosedRange<Int>,
+            minutes: ClosedRange<Int>,
+            seconds: ClosedRange<Int>,
+            frames: ClosedRange<Int>,
+            subFrames: ClosedRange<Int>
+        ) {
+            self.days = days
+            self.hours = hours
+            self.minutes = minutes
+            self.seconds = seconds
+            self.frames = frames
+            self.subFrames = subFrames
+        }
+    }
+}
+
+extension Timecode.ComponentRanges: Equatable { }
+
+extension Timecode.ComponentRanges: Hashable { }
+
+extension Timecode.ComponentRanges: Sendable { }
+
+// MARK: - Static Constructors
+
+extension Timecode.ComponentRanges {
+    /// A default set of unsafe random component value ranges.
+    public static var unsafeRandomRanges = Self(
+        days: 0 ... 99,
+        hours: 0 ... 99,
+        minutes: 0 ... 99,
+        seconds: 0 ... 99,
+        frames: 0 ... 99,
+        subFrames: 0 ... 99
+    )
+}
